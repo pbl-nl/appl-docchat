88a5afec48f9bb145ea92600ebf73c38a4b6d0d5
Troost <troosts@pbl.nl>
2023-09-04 09:23:24 +0200

    cleanup of redundant commented out code

--- query/querier.py
+++ query/querier.py
@@ -2,35 +2,18 @@ from dotenv import load_dotenv
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.memory import ConversationBufferMemory
 from langchain.schema import AIMessage, HumanMessage
 from langchain.vectorstores.chroma import Chroma
 from loguru import logger
 
-# from settings import COLLECTION_NAME, PERSIST_DIRECTORY
-
 
 class Querier:
-    # def __init__(self, input_folder: str, vectordb_folder:str, embeddings_type: str, vectordb_type: str, chunk_size: int, chunk_overlap: int):
-    #     load_dotenv()
-    #     self.input_folder = input_folder
-    #     self.vectordb_folder = vectordb_folder
-    #     self.vectordb_type = vectordb_type
-    #     self.chunk_size = chunk_size
-    #     self.chunk_overlap = chunk_overlap
-    #     self.embeddings_type = embeddings_type
-    #     self.chain = self.make_chain()
-    #     self.chat_history = []
-
     def __init__(self, embeddings_type: str, vectordb_type: str, chunk_size: int, chunk_overlap: int):
         load_dotenv()
-        # self.input_folder = input_folder
-        # self.vectordb_folder = vectordb_folder
         self.vectordb_type = vectordb_type
         self.chunk_size = chunk_size
         self.chunk_overlap = chunk_overlap
         self.embeddings_type = embeddings_type
-        # self.chain = self.make_chain()
         self.chat_history = []
 
     def make_chain(self, input_folder, vectordb_folder):
@@ -55,21 +38,11 @@ class Querier:
             )
             logger.info(f"Loaded chromadb from folder {self.vectordb_folder}")
 
-        # WERKT
         chain = ConversationalRetrievalChain.from_llm(
             llm,
             retriever=vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 4}),
             return_source_documents=True,
         )
-
-        # retriever=vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 4})
-        # memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-        # chain = ConversationalRetrievalChain.from_llm(
-        #     llm=llm,
-        #     retriever=retriever,
-        #     memory=memory,
-        #     return_source_documents=True,
-        # )
 
         logger.info("Executed make_chain(self, input_folder, vectordb_folder)")
         # return chain
