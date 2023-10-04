2e8648231cac027107f818104c74117152e1535b
Troost <troosts@pbl.nl>
2023-10-04 17:45:03 +0200

    different use of settings variables, to make it more explicit that they stem from settings.py

--- ingest.py
+++ ingest.py
@@ -2,16 +2,28 @@ import os
 from loguru import logger
 # local imports
 from ingest.ingester import Ingester
-from settings import VECDB_TYPE, CHUNK_SIZE, CHUNK_OVERLAP, EMBEDDINGS_TYPE
+import settings
 import utils
 
 def main():
     # Get source folder with docs from user
     content_folder_name = input("Source folder of documents (without path): ")
     # get associated source folder path and vectordb path
-    content_folder_path, vectordb_folder_path = utils.create_vectordb_name(content_folder_name)
+    content_folder_path, vectordb_folder_path = utils.create_vectordb_name(content_folder_name, 
+                                                                           settings.DOC_DIR, 
+                                                                           settings.VECDB_DIR, 
+                                                                           settings.VECDB_TYPE, 
+                                                                           settings.EMBEDDINGS_TYPE, 
+                                                                           settings.CHUNK_SIZE, 
+                                                                           settings.CHUNK_OVERLAP)
     if not os.path.exists(vectordb_folder_path):
-        ingester = Ingester(content_folder_name, content_folder_path, vectordb_folder_path, EMBEDDINGS_TYPE, VECDB_TYPE, CHUNK_SIZE, CHUNK_OVERLAP)
+        ingester = Ingester(content_folder_name, 
+                            content_folder_path, 
+                            vectordb_folder_path, 
+                            settings.EMBEDDINGS_TYPE, 
+                            settings.VECDB_TYPE, 
+                            settings.CHUNK_SIZE, 
+                            settings.CHUNK_OVERLAP)
         ingester.ingest()
         logger.info(f"Created Chroma vector store in folder {vectordb_folder_path}")
     else:
--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 import os
-from settings import DOC_DIR, VECDB_DIR, VECDB_TYPE, CHUNK_SIZE, CHUNK_OVERLAP, EMBEDDINGS_TYPE
 
-def create_vectordb_name(content_folder_name):
-    content_folder_path = os.path.join(DOC_DIR, content_folder_name)
-    vectordb_name = "_" + VECDB_TYPE + "_" + str(CHUNK_SIZE) + "_" + str(CHUNK_OVERLAP) + "_" + EMBEDDINGS_TYPE
-    vectordb_folder_path = os.path.join(VECDB_DIR, content_folder_name) + vectordb_name 
+def create_vectordb_name(content_folder_name, doc_dir, vecdb_dir, vecdb_type, embeddings_type, chunk_size, chunk_overlap):
+    content_folder_path = os.path.join(doc_dir, content_folder_name)
+    # vectordb_name is created from vecdb_type, chunk_size, chunk_overlap, embeddings_type 
+    vectordb_name = "_" + vecdb_type + "_" + str(chunk_size) + "_" + str(chunk_overlap) + "_" + embeddings_type
+    vectordb_folder_path = os.path.join(vecdb_dir, content_folder_name) + vectordb_name 
     return content_folder_path, vectordb_folder_path
