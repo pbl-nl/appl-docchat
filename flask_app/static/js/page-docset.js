7e655a210f29df02fcfe0cb857397f7a2d1e87ca
Rob Beffers <rob.beffers@rivm.nl>
2023-10-31 10:55:54 +0100

    Uniformity in page titles

--- flask_app/static/js/page-docset.js
+++ flask_app/static/js/page-docset.js
@@ -1,7 +1,7 @@
 function page_ready() {
     if (parseInt($('#id').val()) >= 1) {
         $('form').find('input,select').each(function(i, el) {
-            var id = $(el.prop('id'));
+            var id = $(el).prop('id');
             if (id != 'id' && id != 'csrf_token') {
                 $(el).attr('disabled', 'disabled');
             }
