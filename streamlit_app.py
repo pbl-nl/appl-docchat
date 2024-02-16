dd1c425dc9470afcbcd191b002cf42362137f1d1
Troost <troosts@pbl.nl>
2024-02-16 16:25:22 +0100

    integration of summary functionality into the streamlit UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,20 +5,52 @@ from loguru import logger
 # local imports
 from ingest.ingester import Ingester
 from query.querier import Querier
+from summarize.summarizer import Summarizer
 import settings
 import utils as ut
 
 
-def click_GO_button():
+def click_go_button():
+    """
+    Sets session state of GO button clicked to True
+    """
     st.session_state['is_GO_clicked'] = True
 
 
-def create_or_update_vectordb(content_folder_name_selected, content_folder_path_selected, vectordb_folder_path_selected):
-    ingester = Ingester(content_folder_name_selected, content_folder_path_selected, vectordb_folder_path_selected)
-    ingester.ingest()
+@st.cache_data
+def create_and_show_summary(my_summary_type,
+                            my_folder_path_selected,
+                            my_folder_name_selected,
+                            my_vectordb_folder_path_selected):
+    summarization_method = "Map_Reduce" if my_summary_type == "Short" else "Refine"
+    # for each file in content folder
+    with st.expander(f"{my_summary_type} summary"):
+        for file in os.listdir(my_folder_path_selected):
+            if os.path.isfile(os.path.join(my_folder_path_selected, file)):
+                summary_name = os.path.join(my_folder_path_selected,
+                                            "summaries",
+                                            file + "_" + str.lower(my_summary_type) + ".txt")
+                # if summary does not exist yet, create it
+                if not os.path.isfile(summary_name):
+                    my_spinner_message = f'''Creating summary for {file}.
+                                        Depending on the size of the file, this may take a while. Please wait...'''
+                    with st.spinner(my_spinner_message):
+                        summarizer = Summarizer(content_folder=my_folder_path_selected,
+                                                collection_name=my_folder_name_selected,
+                                                summary_method=summarization_method,
+                                                vectordb_folder=my_vectordb_folder_path_selected)
+                        summarizer.summarize()
+                # show summary
+                st.write(f"**{file}:**\n")
+                with open(file=summary_name, mode="r", encoding="utf8") as f:
+                    st.write(f.read())
+                    st.divider()
 
 
 def display_chat_history():
+    """
+    Shows the complete chat history
+    """
     for message in st.session_state['messages']:
         if message["role"] != "system":
             with st.chat_message(message["role"]):
@@ -42,52 +74,58 @@ def folderlist_creator():
 
 def folder_selector(folders):
     # Select source folder with docs
-    folder_name_selected = st.sidebar.selectbox("label=folder_selector", options=folders, label_visibility="hidden")
-    logger.info(f"folder_name_selected is now {folder_name_selected}")
+    my_folder_name_selected = st.sidebar.selectbox("label=folder_selector", options=folders, label_visibility="hidden")
+    logger.info(f"folder_name_selected is now {my_folder_name_selected}")
     # get associated source folder path and vectordb path
-    folder_path_selected, vectordb_folder_path_selected = ut.create_vectordb_name(folder_name_selected)
-    logger.info(f"vectordb_folder_path_selected is now {vectordb_folder_path_selected}")
-    if folder_name_selected != st.session_state['folder_selected']:
+    my_folder_path_selected, my_vectordb_folder_path_selected = ut.create_vectordb_name(my_folder_name_selected)
+    logger.info(f"vectordb_folder_path_selected is now {my_vectordb_folder_path_selected}")
+    if my_folder_name_selected != st.session_state['folder_selected']:
         st.session_state['is_GO_clicked'] = False
-    # set session state of selected folder to new source folder 
-    st.session_state['folder_selected'] = folder_name_selected
-    return folder_name_selected, folder_path_selected, vectordb_folder_path_selected
+    # set session state of selected folder to new source folder
+    st.session_state['folder_selected'] = my_folder_name_selected
+    return my_folder_name_selected, my_folder_path_selected, my_vectordb_folder_path_selected
 
 
-def check_vectordb(querier, folder_name_selected, folder_path_selected, vectordb_folder_path_selected):
+def check_vectordb(my_querier, my_folder_name_selected, my_folder_path_selected, my_vectordb_folder_path_selected):
     # If a folder is chosen that is not equal to the last known source folder
     if folder_name_selected != st.session_state['folder_selected']:
         # set session state of is_GO_clicked to False (will be set to True when OK button is clicked)
         st.session_state['is_GO_clicked'] = False
         # clear all chat messages on screen and in Querier object
         st.session_state['messages'] = []
-        querier.clear_history()
-    # When the associated vector database of the chosen content folder doesn't exist with the settings as given in settings.py, create it first
-    if not os.path.exists(vectordb_folder_path_selected):
+        my_querier.clear_history()
+    # When the associated vector database of the chosen content folder doesn't exist with the settings as given
+    # in settings.py, create it first
+    if not os.path.exists(my_vectordb_folder_path_selected):
         logger.info("Creating vectordb")
-        spinner_message = f'Creating vector database for folder {folder_name_selected}. Depending on the size, this may take a while. Please wait...'
+        my_spinner_message = f'''Creating vector database for folder {my_folder_name_selected}.
+        Depending on the size, this may take a while. Please wait...'''
     else:
         logger.info("Updating vectordb")
-        spinner_message = f'Checking if vector database needs an update for folder {folder_name_selected}. This may take a while, please wait...'
-    with st.spinner(spinner_message):
-        create_or_update_vectordb(folder_name_selected, folder_path_selected, vectordb_folder_path_selected)
+        my_spinner_message = f'''Checking if vector database needs an update for folder {my_folder_name_selected}.
+        This may take a while, please wait...'''
+    with st.spinner(my_spinner_message):
+        ingester = Ingester(my_folder_name_selected,
+                            my_folder_path_selected,
+                            my_vectordb_folder_path_selected)
+        ingester.ingest()
 
-    # create a new chain based on the new source folder 
-    querier.make_chain(folder_name_selected, vectordb_folder_path_selected)
-    # set session state of selected folder to new source folder 
-    st.session_state['folder_selected'] = folder_name_selected
+    # create a new chain based on the new source folder
+    my_querier.make_chain(my_folder_name_selected, my_vectordb_folder_path_selected)
+    # set session state of selected folder to new source folder
+    st.session_state['folder_selected'] = my_folder_name_selected
     logger.info("Executed check_vectordb")
 
 
-def handle_query(querier, prompt: str):
+def handle_query(my_querier, my_prompt: str):
     # Display user message in chat message container
     with st.chat_message("user"):
-        st.markdown(prompt)
+        st.markdown(my_prompt)
     # Add user message to chat history
-    st.session_state['messages'].append({"role": "user", "content": prompt})
-    with st.spinner(f'Thinking...'):
+    st.session_state['messages'].append({"role": "user", "content": my_prompt})
+    with st.spinner("Thinking..."):
         # Generate a response
-        response, scores = querier.ask_question(prompt)
+        response, scores = my_querier.ask_question(my_prompt)
     # Display the response in chat message container
     with st.chat_message("assistant"):
         st.markdown(response["answer"])
@@ -97,7 +135,10 @@ def handle_query(querier, prompt: str):
         with st.expander("Paragraphs used for answer"):
             cnt = 0
             for document in response["source_documents"]:
-                st.markdown(f"**page: {document.metadata['page_number']}, chunk: {document.metadata['chunk']}, score: {scores[cnt]:.4f}, file: {document.metadata['filename']}**")
+                st.markdown(f'''**page: {document.metadata['page_number']},
+                            chunk: {document.metadata['chunk']},
+                            score: {scores[cnt]:.4f},
+                            file: {document.metadata['filename']}**''')
                 cnt += 1
                 st.markdown(f"{document.page_content}")
     else:
@@ -118,7 +159,7 @@ def initialize_page():
     with headercol:
         st.header(settings.APP_HEADER)
     # set session state default for messages to fight hallucinations
-    # st.session_state.setdefault('messages', [{"role": "system", "content": "You are a helpful assistant. 
+    # st.session_state.setdefault('messages', [{"role": "system", "content": "You are a helpful assistant.
     # Custom CSS to have white expander background
     st.markdown(
         '''
@@ -137,8 +178,8 @@ def initialize_page():
     )
     with st.sidebar.expander("User manual"):
         # read app explanation from file explanation.txt
-        with open(file=settings.APP_INFO) as file:
-            explanation = file.read()
+        with open(file=settings.APP_INFO, mode="r", encoding="utf8") as f:
+            explanation = f.read()
         st.markdown(body=explanation, unsafe_allow_html=True)
         st.image("./images/multilingual.png")
     st.sidebar.divider()
@@ -161,49 +202,54 @@ def initialize_querier():
     """
     Create a Querier object
     """
-    querier = Querier()
+    my_querier = Querier()
     logger.info("Executed initialize_querier()")
-    return querier
+    return my_querier
 
 
 def set_page_config():
-    st.set_page_config(page_title="Chat with your documents", page_icon=':books:', layout='wide', initial_sidebar_state='auto')
+    st.set_page_config(page_title="Chat with your documents",
+                       page_icon=':books:',
+                       layout='wide',
+                       initial_sidebar_state='auto')
     logger.info("\nExecuted set_page_config()")
 
 
-
-
-#### MAIN PROGRAM ####
+# ### MAIN PROGRAM ####
 # set page configuration, this is the first thing that needs to be done
 set_page_config()
-# Initialize page, executed only once per session
+# initialize page, executed only once per session
 initialize_page()
 # create list of content folders
 source_folders_available = folderlist_creator()
 # initialize session state variables
 initialize_session_state()
-# Creation of Querier object, executed only once per session
+# creation of Querier object, executed only once per session
 querier = initialize_querier()
-# Chosen folder and associated vector database
+# chosen folder and associated vector database
 folder_name_selected, folder_path_selected, vectordb_folder_path_selected = folder_selector(source_folders_available)
 
-# Create button to confirm folder selection. This button sets session_state['is_GO_clicked'] to True
-st.sidebar.button("GO", type="primary", on_click=click_GO_button)
+# create button to confirm folder selection. This button sets session_state['is_GO_clicked'] to True
+st.sidebar.button("GO", type="primary", on_click=click_go_button)
 
-# Only start a conversation when a folder is selected and selection is confirmed with "GO" button
+# only start a conversation when a folder is selected and selection is confirmed with "GO" button
 if st.session_state['is_GO_clicked']:
     # create or update vector database if necessary
     check_vectordb(querier, folder_name_selected, folder_path_selected, vectordb_folder_path_selected)
-    # If a summary is required
-    chk_summary = st.sidebar.checkbox(label="show summary")
-    if chk_summary:
-        with st.sidebar.expander("Summary"):
-            st.markdown(body="summary comes here", unsafe_allow_html=True)
+    summary_type = st.sidebar.radio(
+        "Start with summary?",
+        ["No", "Short", "Long"],
+        captions=["No, start the conversation", "Quick but lower quality", "Slow but higher quality"],
+        index=0)
+    # if a short or long summary is chosen
+    if summary_type in ["Short", "Long"]:
+        # show the summary at the top of the screen
+        create_and_show_summary(summary_type, folder_path_selected, folder_name_selected, vectordb_folder_path_selected)
 
-    # Show button "Clear Conversation"
+    # show button "Clear Conversation"
     clear_messages_button = st.button("Clear Conversation", key="clear")
-    
-    # If button "Clear Conversation" is clicked
+
+    # if button "Clear Conversation" is clicked
     if clear_messages_button:
         # clear all chat messages on screen and in Querier object
         # NB: session state of "is_GO_clicked" and "folder_selected" remain unchanged
@@ -211,9 +257,9 @@ if st.session_state['is_GO_clicked']:
         querier.clear_history()
         logger.info("Clear Conversation button clicked")
 
-    # Display chat messages from history 
+    # display chat messages from history
     display_chat_history()
 
-    # React to user input if a question has been asked
+    # react to user input if a question has been asked
     if prompt := st.chat_input("Your question"):
         handle_query(querier, prompt)
