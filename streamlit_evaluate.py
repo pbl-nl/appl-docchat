44d785b8192cbfca2528ffe1384a4582d534de4f
Troost <troosts@pbl.nl>
2024-06-19 15:51:58 +0200

    rework on evaluation module and UI and inclusion of evaluation file name in results

--- streamlit_evaluate.py
+++ streamlit_evaluate.py
@@ -1,10 +1,11 @@
 import os
+from typing import List, Tuple
 import streamlit as st
-from PIL import Image
 from loguru import logger
+import pandas as pd
 # local imports
 import settings
-import pandas as pd
+import utils as ut
 
 
 def folderlist_creator():
@@ -12,43 +13,69 @@ def folderlist_creator():
     Creates a list of folder names
     Evaluation folder names are found in evaluation output files in folder /evaluate
     """
-    folders = [f[:-8] for f in os.listdir(settings.EVAL_DIR) if 
+    folders = [f[:-8] for f in os.listdir(settings.EVAL_DIR) if
                (os.path.isfile(os.path.join(settings.EVAL_DIR, f)) and f.endswith("_agg.tsv"))]
     logger.info("Executed evaluation folderlist_creator()")
+
     return folders
 
 
-def compose_dataframes_from_all_eval_files(eval_folders):
+def compose_dataframes_from_all_eval_files(eval_folders: List[str]) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    creates aggregated and detailed dataframe to show in the evaluation UI
+
+    Parameters
+    ----------
+    eval_folders : List[str]
+        list of folders with evaluation results
+
+    Returns
+    -------
+    Tuple[pd.DataFrame, pd.DataFrame]
+        dataframe with aggregated results and dataframe with detailed results
+    """
     found_eval_folder = False
     for eval_folder in eval_folders:
         eval_agg_file_name = os.path.join(settings.EVAL_DIR, eval_folder + "_agg.tsv")
         eval_file_name = os.path.join(settings.EVAL_DIR, eval_folder + ".tsv")
         if not found_eval_folder:
-            df_eval_agg = pd.read_csv(eval_agg_file_name, sep="\t")
-            df_eval = pd.read_csv(eval_file_name, sep="\t")
+            df_agg = pd.read_csv(eval_agg_file_name, sep="\t")
+            df = pd.read_csv(eval_file_name, sep="\t")
             found_eval_folder = True
         else:
-            df_eval_agg = pd.concat([df_eval_agg, pd.read_csv(eval_agg_file_name, sep="\t")], axis=0)
-            df_eval = pd.concat([df_eval, pd.read_csv(eval_file_name, sep="\t")], axis=0)
-    df_eval_agg = df_eval_agg.sort_values(by="timestamp", ascending=False)
-    df_eval = df_eval.sort_values(by="timestamp", ascending=False)
+            df_agg = pd.concat([df_agg, pd.read_csv(eval_agg_file_name, sep="\t")], axis=0)
+            df = pd.concat([df, pd.read_csv(eval_file_name, sep="\t")], axis=0)
+    df_agg = df_agg.sort_values(by="timestamp", ascending=False)
+
+    admin_columns = ["folder", "timestamp", "eval_file"]
+    result_columns = ["answer_relevancy", "context_precision", "faithfulness", "context_recall"]
+    settings_dict = ut.get_settings_as_dictionary("settings.py")
+    settings_columns = list(settings_dict.keys())
+
+    # force order of columns for aggregated results
+    df_agg = df_agg.loc[:, admin_columns + result_columns + settings_columns]
+    df = df.sort_values(by="timestamp", ascending=False)
+
+    # force order of columns for detailed results
+    df = df.loc[:, admin_columns + ["question", "ground_truth", "answer", "contexts"] + result_columns]
+
     # from https://docs.streamlit.io/knowledge-base/using-streamlit/how-to-get-row-selections
-    df_eval_agg_selections = df_eval_agg.copy()
-    df_eval_agg_selections.insert(0, "Select", False)
+    df_agg_selections = df_agg.copy()
+    df_agg_selections.insert(0, "Select", False)
     # Get dataframe row-selections from user with st.data_editor
-    df_eval_agg_ed = st.data_editor(
-        df_eval_agg_selections,
+    df_agg_ed = st.data_editor(
+        df_agg_selections,
         hide_index=True,
         column_config={"Select": st.column_config.CheckboxColumn(required=True)},
-        disabled=df_eval_agg.columns,
+        disabled=df_agg.columns,
     )
     # store selected rows and use these to create list of timestamps
-    selected_rows = df_eval_agg_ed[df_eval_agg_ed.Select]
+    selected_rows = df_agg_ed[df_agg_ed.Select]
     selected_timestamps = list(selected_rows["timestamp"])
     # use selected timestamps as filter for detailed results
-    df_eval = df_eval.loc[df_eval["timestamp"].isin(selected_timestamps), :]
-    return df_eval_agg_ed, df_eval
+    df = df.loc[df["timestamp"].isin(selected_timestamps), :]
 
+    return df_agg_ed, df
 
 
 @st.cache_data
@@ -56,15 +83,10 @@ def initialize_page():
     """
     Initializes the main page with a page header and app info
     """
-    imagecol, headercol = st.columns([0.3, 0.7])
-    logo_image = Image.open(settings.APP_LOGO)
-    with imagecol:
-        st.image(logo_image, width=250)
-    with headercol:
-        st.header(settings.EVAL_APP_HEADER)
-    with st.expander("Show explanation of evaluation metrics"):
+    st.header(settings.EVAL_APP_HEADER)
+    with st.expander(label="Show explanation of evaluation metrics"):
         # read app explanation from file evaluation_explanation.txt
-        with open(file=settings.EVAL_APP_INFO) as file:
+        with open(file=settings.EVAL_APP_INFO, encoding="utf8") as file:
             eval_explanation = file.read()
         _, cent_co, _ = st.columns(3)
         with cent_co:
@@ -75,17 +97,20 @@ def initialize_page():
 
 
 def set_page_config():
+    """
+    sets the page configuration
+    """
     st.set_page_config(page_title="ChatNMDC evaluation", page_icon=':books:', layout='wide')
     logger.info("\nExecuted set_page_config()")
 
 
-
-
-#### MAIN PROGRAM ####
+# ### MAIN PROGRAM ####
 # set page configuration, this is the first thing that needs to be done
 set_page_config()
+
 # Initialize page, executed only once per session
 initialize_page()
+
 # create list of content folders
 evaluation_folders_available = folderlist_creator()
 
