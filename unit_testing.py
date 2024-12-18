3af176032f5804bf9fc90de477c4b4df2beb704e
ChrisCleaner <christoph.kruger@yahoo.com>
2024-12-18 20:04:43 +0100

    stashing

--- unit_testing.py
+++ unit_testing.py
@@ -1,5 +1,5 @@
-from unit_testing.ingest_test import TestQuery, TestIngester
-import unittest
+# from unit_testing.ingest_test import TestQuery, TestIngester
+# import unittest
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
