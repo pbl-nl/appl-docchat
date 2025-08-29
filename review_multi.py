d169db5bfaf43f1f26024b904df54a555d26dc45
Krüger <krugerc@pbl.nl>
2025-08-29 11:24:20 +0200

    added time delay, to possibly prevent crashing of ChromaDB

--- review_multi.py
+++ review_multi.py
@@ -4,6 +4,7 @@ This module is meant to use the review.py module repetitively
 import review
 import time
 
+
 if __name__ == "__main__":
     # get source folder with papers from user
     content_folder_path = input("Source folder of documents (including path): ")
